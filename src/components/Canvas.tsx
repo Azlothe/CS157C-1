748865852e4187fd185b92122d9adb60c023ca9f
David Sang <dvdsang47@gmail.com>
2024-04-02 23:49:10 -0700

    fix strokes and center state loop

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import {
     P5CanvasInstance,
     ReactP5Wrapper,
@@ -10,86 +10,141 @@ const HEIGHT = 450;
 const BG_COLOR = {
     r: 255,
     g: 255,
-    b: 255
+    b: 255,
 };
 
 interface CustomSketchProps extends SketchProps {
+    pcenter: { x: number; y: number };
     updateCenter: (newCenter: { x: number; y: number }) => void;
+    isMouseMove: boolean;
 }
 
 function sketch(p5: P5CanvasInstance<CustomSketchProps>) {
     const center = { x: 0, y: 0 };
+    let isP5Init = false;
+    let isPan = false;
+    let isDraw = true;
 
     // only runs once on mount
     p5.setup = () => {
         p5.createCanvas(WIDTH, HEIGHT, p5.WEBGL);
-        // p5.background(BG_COLOR.r, BG_COLOR.g, BG_COLOR.b);
+        p5.background(BG_COLOR.r, BG_COLOR.g, BG_COLOR.b);
+        p5.frameRate(60);
+        isP5Init = true;
     };
 
     // loops continuously
     p5.draw = () => {
-        p5.background(BG_COLOR.r, BG_COLOR.g, BG_COLOR.b);
-        p5.noStroke();
+        if (isPan) {
+            p5.background(BG_COLOR.r, BG_COLOR.g, BG_COLOR.b);
+        }
 
         // panning
         p5.translate(center.x, center.y);
 
-        // mouse cursor
-        p5.fill(0, 0, 0);
-        const mouseXOffset = p5.mouseX - WIDTH / 2 - center.x;
-        const mouseYOffset = p5.mouseY - HEIGHT / 2 - center.y;
-        p5.circle(mouseXOffset, mouseYOffset, 10);
+        p5.noStroke();
 
         // draw elements in sorted order
         p5.fill(0, 51, 160); // the color of the element
         p5.circle(0, 0, 100); // the position and size of the element
 
         p5.fill(0, 51, 0);
-        p5.circle(-200, -200, 100);
+        p5.circle(-150, -150, 100);
 
         p5.fill(255, 0, 0);
-        p5.circle(200, 200, 100);
+        p5.circle(150, 150, 100);
     };
 
     p5.mouseDragged = () => {
-        // case: Tool = "Pan"
-        center.x += p5.mouseX - p5.pmouseX; // dX
-        center.y += p5.mouseY - p5.pmouseY; // dY
+        // Tool = "Pan"
+        if (isP5Init && isPan) {
+            center.x += p5.mouseX - p5.pmouseX; // dX
+            center.y += p5.mouseY - p5.pmouseY; // dY
+        }
 
-        // case: Tool = "Brush"
-        // p5.fill(0, 0, 0);
-        // const mouseXOffset = p5.mouseX - WIDTH / 2 - center.x;
-        // const mouseYOffset = p5.mouseY - HEIGHT / 2 - center.y;
-        // p5.circle(mouseXOffset, mouseYOffset, 10);
+        // Tool = "Brush"
+        if (isP5Init && isDraw) {
+            p5.stroke(0, 0, 0);
+            p5.strokeWeight(10);
+            const pmouseXOffset = p5.pmouseX - WIDTH / 2 - center.x;
+            const pmouseYOffset = p5.pmouseY - HEIGHT / 2 - center.y;
+            const mouseXOffset = p5.mouseX - WIDTH / 2 - center.x;
+            const mouseYOffset = p5.mouseY - HEIGHT / 2 - center.y;
+            p5.line(pmouseXOffset, pmouseYOffset, mouseXOffset, mouseYOffset);
+        }
+    };
+
+    p5.keyPressed = () => {
+        if (p5.key == " ") {
+            isPan = true;
+            isDraw = false;
+        }
+    };
+
+    p5.keyReleased = () => {
+        if (p5.key == " ") {
+            isPan = false;
+            isDraw = true;
+        }
     };
 
     p5.updateWithProps = (props) => {
-        props.updateCenter({ x: -center.x, y: -center.y });
+        // to fix negative zeros 
+        const newX = center.x === 0 ? 0 : -center.x;
+        const newY = center.y === 0 ? 0 : -center.y;
+        
+        if (isPan && props.isMouseMove) {
+            console.log("update center state"); 
+            if (newX !== props.pcenter.x && newY !== props.pcenter.y) {
+                props.updateCenter({ x: newX, y: newY });
+            }
+        }
     };
 }
 
 function Canvas() {
     const [center, setCenter] = useState({ x: 0, y: 0 });
-    const [bottomRight, setBottomRight] = useState({ x: WIDTH, y: HEIGHT });
+    const [isMouseDown, setIsMouseDown] = useState(false);
+    const [isMouseMove, setIsMouseMove] = useState(false);
+
+    const bottomRight = {
+        x: center.x + Math.ceil(WIDTH / 2),
+        y: center.y + Math.ceil(HEIGHT / 2),
+    };
 
     const updateCenter = (newCenter: { x: number; y: number }) => {
         setCenter(newCenter);
     };
 
-    useEffect(() => {
-        setBottomRight({ x: center.x + Math.ceil(WIDTH / 2), y: center.y + Math.ceil(HEIGHT / 2) });
-    }, [center]);
+    const handleMouseDown = () => {
+        setIsMouseDown(true);
+    };
 
-    return (
+    const handleMouseUp = () => {
+        setIsMouseDown(false);
+    }; 
+
+    const handleMouseMove = () => {
+        if (isMouseDown) {
+            setIsMouseMove(!isMouseMove);
+        }
+    };
+ 
+    return ( 
         <>
-            <div>
+            <div onMouseDown={handleMouseDown} onMouseUp={handleMouseUp} onMouseMove={handleMouseMove}> 
                 <p>
-                    Center: ({center.x}, {center.y})
+                    Center: ({center.x}, {center.y}) 
                 </p>
                 <p>
                     Bottom Right: ({bottomRight.x}, {bottomRight.y})
                 </p>
-                <ReactP5Wrapper sketch={sketch} updateCenter={updateCenter} />
+                <ReactP5Wrapper
+                    sketch={sketch}
+                    pcenter={center}
+                    updateCenter={updateCenter}
+                    isMouseMove={isMouseMove}
+                />
             </div>
         </>
     );
