53de4f6e3fbc8731ebee2eeda5f456bf7ea627fe
Azlothe <bbqian7@gmail.com>
2024-04-03 22:35:47 -0700

    separate database and server init

---
+++ server/database/connection.mjs
@@ -0,0 +1,8 @@
+import CONFIG from './dbConfig.mjs';
+import cassandra from "cassandra-driver";
+
+export const client = new cassandra.Client({
+    contactPoints: CONFIG.CONTACT_POINTS,
+    localDataCenter: CONFIG.DATA_CENTER,
+    keyspace: CONFIG.KEYSPACE,
+});
---
+++ server/database/dbConfig.mjs
@@ -0,0 +1,6 @@
+
+export default {
+    CONTACT_POINTS: ['127.0.0.1'],
+    DATA_CENTER: 'datacenter1',
+    KEYSPACE: 'emp'
+}
