2ae3b3a8d74bf3334e707de9571328a1e0b239e3
Azlothe <bbqian7@gmail.com>
2024-04-03 21:53:50 -0700

    clean up testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CanvasPage from "./pages/CanvasPage";
 import HomePage from "./pages/HomePage";
-import { useEffect, useState } from "react";
 
 function App() {
-  // // console.log(test.testingFunc().then(result => console.log(result)));
-  // useEffect(() => {
-  //     testingFunc();
-  // }, []);
-
-  const [employees, setEmployees] = useState([]);
-
-  useEffect(() => {
-    console.log(`${import.meta.env.VITE_SERVER}/api/emp`);
-    fetch(`${import.meta.env.VITE_SERVER}/api/emp`)
-      .then((response) => response.json())
-      .then((data) => setEmployees(data))
-      .catch((error) => console.error("Error fetching data:", error));
-  }, []);
-
   return (
     <>
       <BrowserRouter>
@@ -27,14 +11,6 @@ function App() {
           <Route path="/canvas" element={<CanvasPage />} />
         </Routes>
       </BrowserRouter>
-      <div>
-        <h1>Vehicles</h1>
-        <ul>
-          {employees.map((emp) => (
-            <li key={emp.emp_id}>{emp.emp_name}</li>
-          ))}
-        </ul>
-      </div>
     </>
   );
 }
