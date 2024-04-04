45c08fb24cc944ddf9e8408c412f3fb5a5dec27f
Azlothe <bbqian7@gmail.com>
2024-04-03 21:49:39 -0700

    testing loading data

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,42 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CanvasPage from "./pages/CanvasPage";
 import HomePage from "./pages/HomePage";
+import { useEffect, useState } from "react";
 
 function App() {
-    return (
-        <>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element = {<HomePage />}/>
-                    <Route path="/canvas" element={<CanvasPage />} />
-                </Routes>
-            </BrowserRouter>
-        </>
-    );
+  // // console.log(test.testingFunc().then(result => console.log(result)));
+  // useEffect(() => {
+  //     testingFunc();
+  // }, []);
+
+  const [employees, setEmployees] = useState([]);
+
+  useEffect(() => {
+    console.log(`${import.meta.env.VITE_SERVER}/api/emp`);
+    fetch(`${import.meta.env.VITE_SERVER}/api/emp`)
+      .then((response) => response.json())
+      .then((data) => setEmployees(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/canvas" element={<CanvasPage />} />
+        </Routes>
+      </BrowserRouter>
+      <div>
+        <h1>Vehicles</h1>
+        <ul>
+          {employees.map((emp) => (
+            <li key={emp.emp_id}>{emp.emp_name}</li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default App;
