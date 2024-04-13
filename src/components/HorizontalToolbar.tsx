5977466f9f7262171468daa7f921692000c64727
David Sang <dvdsang47@gmail.com>
2024-04-12 18:47:33 -0700

    add logout button

--- src/components/HorizontalToolbar.tsx
+++ src/components/HorizontalToolbar.tsx
@@ -1,18 +1,28 @@
 import Toolbar from "@mui/material/Toolbar";
+import { Button } from "@/components/ui/button";
 
 interface Props {
     center: { x: number; y: number };
 }
 
 function HorizontalToolbar({ center }: Props) {
+    const handleLogout = async () => {
+        try {
+            // call logout api endpoint
+            // if logout successful, redirect to home page
+            // else handle error
+        } catch (error) {
+            console.error("Logout Error:", error);
+        }
+    };
+
     return (
         <>
             <Toolbar
-                variant="dense"
                 sx={{
                     backgroundColor: "rgba(255, 184, 26, 0)",
                     flexDirection: "row",
-                    justifyContent: "left",
+                    justifyContent: "space-between",
                     alignItems: "center",
                     width: "100vw",
                     position: "fixed",
@@ -23,6 +33,7 @@ function HorizontalToolbar({ center }: Props) {
             >
                 <b
                     style={{
+                        display: "block",
                         marginLeft: "-12px",
                         fontFamily: "Montserrat, sans-serif",
                         textShadow:
@@ -31,6 +42,9 @@ function HorizontalToolbar({ center }: Props) {
                 >
                     ({center.x}, {center.y})
                 </b>
+                <Button className="mr-[-12px]" onClick={handleLogout}>
+                    Log out
+                </Button>
             </Toolbar>
         </>
     );
