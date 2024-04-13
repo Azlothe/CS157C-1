b664688268c920007d1463f6588c1c17013c37d5
Azlothe <bbqian7@gmail.com>
2024-04-13 10:28:59 -0700

    add links to home

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,6 +1,11 @@
 import logo from "/spartandraw.svg";
 import styles from "./HomePage.module.css"
 
+const Links = {
+  Login: "/login",
+  Canvas: "/canvas"
+}
+
 const HomePage = () => {
   return (
     <>
@@ -9,8 +14,10 @@ const HomePage = () => {
         <img src={logo} className="w-14 mx-2"></img>
         <h1 className={styles.title}>SpartanDraw</h1>
       </div>
-      <div className="text-center m-8">
-        <a href="/login" className={styles.link}>Go to Login</a>
+      <div className="text-center m-4 flex flex-col">
+        {
+          Object.entries(Links).map( ([key, val]) => <a href={val} className={styles.link}>Go to {key}</a>)
+        }
       </div>
     </div>
     </>
