3c7c3b0f2b42823a94e1c3f771f23507dbb3fb64
Azlothe <bbqian7@gmail.com>
2024-04-13 10:01:57 -0700

    fix theme and separated css into module

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,16 +1,18 @@
 import logo from "/spartandraw.svg";
-import "./HomePage.css"
+import styles from "./HomePage.module.css"
 
 const HomePage = () => {
   return (
     <>
-      <div className="w-screen flex flex-row justify-center">
+    <div className={styles.body}>
+      <div className={styles.bg}>
         <img src={logo} className="w-14 mx-2"></img>
-        <h1 className="text-gray-100">SpartanDraw</h1>
+        <h1 className={styles.title}>SpartanDraw</h1>
       </div>
       <div className="text-center m-8">
-        <a href="/login" className="bg-gray-100 p-3 rounded-lg">Go to Login</a>
+        <a href="/login" className={styles.link}>Go to Login</a>
       </div>
+    </div>
     </>
   );
 };
