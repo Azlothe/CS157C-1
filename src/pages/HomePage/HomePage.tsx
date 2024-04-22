fb3a9bc251d263599836417f40c4846970f811e3
Azlothe <bbqian7@gmail.com>
2024-04-21 23:39:32 -0700

    set new name or default string for home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,8 +2,13 @@ import logo from "/spartandraw.svg";
 import styles from "./HomePage.module.css"
 
 const Links = {
-  Login: "/login",
-  Canvas: "/canvas"
+  Login: {
+    link: "/login",
+  },
+  Canvas: {
+    link: "/canvas",
+    name: "View Canvas as Guest",
+  },
 }
 
 const HomePage = () => {
@@ -16,7 +21,7 @@ const HomePage = () => {
       </div>
       <div className="text-center m-4 flex flex-col">
         {
-          Object.entries(Links).map( ([key, val]) => <a href={val} className={styles.link}>Go to {key}</a>)
+          Object.entries(Links).map( ([key, val]) => <a href={val.link} className={styles.link}>{val.name ?? `Go to ${key}`}</a>)
         }
       </div>
     </div>
