e63913112c3e40c6bb502e62bd41f1f7bf7b3acc
Azlothe <bbqian7@gmail.com>
2024-04-23 13:47:27 -0700

    add reset shortcut

--- server/database/initDB.mjs
+++ server/database/initDB.mjs
@@ -5,6 +5,13 @@ import createBlacklist from "./models/blacklist/blacklist.mjs";
 import createStrokes from "./models/strokes/strokes.mjs"
 import createUserInfo from "./models/users/users.mjs";
 
+// Run to reset database!
+const resetDB = async() => {
+    const query = `DROP KEYSPACE IF EXISTS ${CONSTANTS.KEYSPACE};`
+    await client.execute(query);
+    console.log("database has been reset");
+}
+
 const createKeyspace = async () => {
     const query = `CREATE KEYSPACE IF NOT EXISTS ${CONSTANTS.KEYSPACE} WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };`
     await client.execute(query);
@@ -21,6 +28,7 @@ const createTables = async () => {
 }
 
 export async function initDB() {
+    // await resetDB();
     await createKeyspace();
     await createTables();
 
